11360b1b031d9378848494eddce6428888340051
Saurav Paudel <Sauravpaudel22@gmail.com>
2024-02-17 10:06:24 +0545

    comit

--- Pyramid/src/App.jsx
+++ Pyramid/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, Navigate} from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
 import Home from './components/pages/Home';
 import Project from './components/pages/Project';
@@ -7,17 +7,19 @@ import Teams from './components/pages/Teams'
 import About from './components/pages/About'
 import Footer from './components/Footer/Footer';
 
+
 function App(){
 return(
   <>
-   
+ 
     <Navbar/>
     <Routes> 
-      <Route path="/Home"  element={<Home/>} />
+      <Route path="/"  element={<Home/>} />
       <Route path="/Project"  element={<Project />}/>
       <Route path="/Services"  element={<Services/>}/>
       <Route path="/Teams" element ={<Teams/>}/>
       <Route path="/About" element ={<About/>}/>
+      <Route path='*' element={<Navigate to='/' />} />
       
     </Routes>
   <Footer/>
